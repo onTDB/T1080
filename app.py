2f4df7aa6aee4db2a3e7f71231f37446482c480a
onTDB <github@ontdb.com>
2022-10-31 10:46:36 +0900

    Add ProxyFix & gc

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-from flask import *
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_cors import CORS
-import os
+from flask import Flask
 from streamlink import streams
+from gc import collect
 import requests
+import os
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
 CORS(app)
 
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
 @app.route('/<path:path>')
 def static_file(path):
     s = streams("https://www.twitch.tv/" + path.replace(".m3u8", ""))
     if s:
-        return requests.get(s["best"].url_master).content, 200
+        rtn = requests.get(s["best"].url_master).content
+        status = 200
     else:
-        return "Can not find channel", 404
+        rtn = "Can not find channel"
+        status = 404
+    
+    del(s)
+    collect()
+    return rtn, status
 
 if __name__ == '__main__':
     app.debug = True
